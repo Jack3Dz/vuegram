2285f1606f0bec40bcddf238aa345d4dfb55c3d0
jefferson.bruchado <jefferson.bruchado@neoway.com.br>
2019-03-15 12:38:21 -0300

    changing to main import scss

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import firebase from 'firebase'
-import './assets/scss/app.scss'
 
 import Login from '@/components/Login'
 import Dashboard from '@/components/Dashboard'
