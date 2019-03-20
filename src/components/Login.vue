21b7f1f92a243ea972652f20a7f3305abe3cc81e
jefferson.bruchado <jefferson.bruchado@neoway.com.br>
2019-03-20 12:05:29 -0300

    implementing forgot password and fixing some bugs

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -11,7 +11,7 @@
                 <p>Welcome to the <a href="https://savvyapps.com/" target="_blank">Savvy Apps</a> sample social media web app powered by Vue.js and Firebase.
                     Build this project by checking out The Definitive Guide to Getting Started with Vue.js</p>
             </div>
-            <div class="col2" :class="{ 'signup-form': !showLoginForm }">
+            <div class="col2" :class="{ 'signup-form': !showLoginForm && !showForgotPassword }">
                 <form v-if="showLoginForm" @submit.prevent>
                     <h1>Welcome Back</h1>
 
@@ -28,7 +28,7 @@
                         <a @click="toggleForm">Create an Account</a>
                     </div>
                 </form>
-                <form v-else @submit.prevent>
+                <form v-if="!showLoginForm && !showForgotPassword" @submit.prevent>
                     <h1>Get Started</h1>
 
                     <label for="name">Name</label>
@@ -47,6 +47,26 @@
 
                     <div class="extras">
                         <a @click="toggleForm">Back to Log In</a>
+                    </div>
+                </form>
+                <form v-if="showForgotPassword" @submit.prevent class="password-reset">
+                    <div v-if="!passwordResetSuccess">
+                        <h1>Reset Password</h1>
+                        <p>We will send you an email to reset your password</p>
+
+                        <label for="email3">Email</label>
+                        <input v-model.trim="passwordForm.email" type="text" placeholder="you@email.com" id="email3" />
+
+                        <button @click="resetPassword" class="button">Submit</button>
+
+                        <div class="extras">
+                            <a @click="togglePasswordReset">Back to Log In</a>
+                        </div>
+                    </div>
+                    <div v-else>
+                        <h1>Email Sent</h1>
+                        <p>check your email for a link to reset your password</p>
+                        <button @click="togglePasswordReset" class="button">Back to login</button>                        
                     </div>
                 </form>
                 <transition name="fade">
@@ -75,19 +95,35 @@
                     email: '',
                     password: ''
                 },
+                passwordForm: {
+                    email: ''
+                },
                 showLoginForm: true,
+                showForgotPassword: false,
+                passwordResetSuccess: false,
                 perfomingRequest: false,
                 errorMsg: ''
             }
         },
         methods: {
             toggleForm() {
+                this.errorMsg = ''
                 this.showLoginForm = !this.showLoginForm
+            },
+            togglePasswordReset() {
+                if (this.showForgotPassword) {
+                    this.showLoginForm = true
+                    this.showForgotPassword = false
+                    this.passwordResetSuccess = false
+                } else {
+                    this.showLoginForm = false
+                    this.showForgotPassword = true
+                }
             },
             login() {
                 this.perfomingRequest = true
 
-                fb.auth.signInWithEmailAndPassword(this.loginForm.email, this.login.password).then(user => {
+                fb.auth.signInWithEmailAndPassword(this.loginForm.email, this.loginForm.password).then(user => {
                     this.$store.commit('setCurrentUser', user.user)
                     this.$store.dispatch('fetchUserProfile')
                     this.perfomingRequest = false
@@ -119,8 +155,23 @@
                     })
                 }).catch(err => {
                     console.log(err)
+                    this.perfomingRequest = false
+                })
+            },
+            resetPassword() {
+                this.perfomingRequest = true
+
+                fb.auth.sendPasswordResetEmail(this.passwordForm.email).then(() => {
+                    this.perfomingRequest = false
+                    this.passwordResetSuccess = true
+                    this.passwordForm.email = ''
+                }).catch(err => {
+                    console.log(err)
+                    this.perfomingRequest = false
+                    this.errorMsg = err.message
                 })
             }
+            
         }
     }
 </script>
