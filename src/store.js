cc2515dab16f601b9ca05f43a354569b83a12028
jefferson.bruchado <jefferson.bruchado@neoway.com.br>
2019-03-20 12:37:41 -0300

    implementing logout

--- src/store.js
+++ src/store.js
@@ -5,12 +5,23 @@ Vue.use(Vuex)
 
 const fb = require('@/firebaseConfig.js');
 
+fb.auth.onAuthStateChanged(user => {
+    if (user) {
+        store.commit('setCurrentUser', user)
+        store.dispatch('fetchUserProfile')
+    }
+})
+
 export const store = new Vuex.Store({
     state: {
         currentUser: null,
         userProfile: {}
     },
     actions: {
+        clearData({ commit }) {
+            commit('setCurrentUser', null)
+            commit('setUserProfile', {})
+        },
         fetchUserProfile({ commit, state }) {
             fb.usersCollection.doc(state.currentUser.uid).get().then(res => {
                 commit('setUserProfile', res.data())
