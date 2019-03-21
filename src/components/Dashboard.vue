9ce6a75d8c68c475a47a0c00d5cf8b37a3308a72
jefferson.bruchado <jefferson.bruchado@neoway.com.br>
2019-03-21 11:04:26 -0300

    creating dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,11 +1,57 @@
 <template>
-    <div>
-        
+    <div id="dashboard">
+        <section>
+            <div class="col1">
+                <div class="profile">
+                    <h5>{{ userProfile.name }}</h5>
+                    <p>{{ userProfile.title }}</p>
+                    <div class="create-post">
+                        <p>create a post</p>
+                        <form @submit.prevent>
+                            <textarea></textarea>
+                            <button @click="createPost" :disabled="post.content == ''" class="button">post</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col2">
+                <div>
+                    <p class="no-results">There are currently no posts</p>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
 <script>
+    import { mapState } from "vuex";
+
     export default {
-        
+        data() {
+            return {
+                post: {
+                    content: ''
+                }
+            }
+        },
+        computed: {
+            ...mapState(['userProfile'])
+        },
+        methods: {
+            createPost() {
+                fb.postsCollection.add({
+                    createdOn: new Data(),
+                    content: this.post.content,
+                    userId: this.currentUser.uid,
+                    userName: this.userProfile.name,
+                    comments: 0,
+                    likes: 0
+                }).then(ref => {
+                    this.post.content = ''
+                }).catch(err => {
+                    console.log(err);
+                })
+            }
+        }
     }
 </script>
